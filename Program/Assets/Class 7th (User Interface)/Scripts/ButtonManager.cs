ea9602a8226fde23f95d96910d6e7cfa115265a4
jinjae84 <jinjae84@naver.com>
2025-01-21 21:43:26 +0900

    (2025.01.21) 유저 인터페이스

---
+++ Program/Assets/Class 7th (User Interface)/Scripts/ButtonManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    [SerializeField] Text[ ] textList;
+
+    [SerializeField] string[ ] titleList;
+
+    void Start()
+    {
+        SetText();
+    }
+
+    public void SetText()
+    {
+        for(int i = 0; i <textList.Length; i++)
+        {
+            textList[i].text = titleList[i];
+        }
+    }
+
+    public void Execute()
+    {
+        Debug.Log("Execute");
+    }
+
+    public void Shop()
+    {
+        Debug.Log("Shop");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+    }
+}
